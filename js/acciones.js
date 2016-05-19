925c215c0c1b1444b309a247009d29ebc2f8c07e
macamalpica <abril.dsaupoo@gmail.com>
2016-05-19 07:40:14 -0500

    10subida
    app9

--- js/acciones.js
+++ js/acciones.js
@@ -1,60 +1,57 @@
 // JavaScript Document
 $(document).ready(function (e){
-	//watchID se refiere a actuar
+    //watchID se refiere a actual
 	
-	var watchID=null;
-	document.addEventListener("deviceready",Dispositivo_Listo,false);
+var watchID=null;
+document.addEventListener("deviceready",Dispositivo_Listo,false);
 	
-	//Cuando está listo el dispositivo
-	function Dispositivo_Listo(){
-		Comienza();
+	//Cuando esta listo el dispositivo
+function Dispositivo_Listo(){
+Comienza();
 	}
 	
-//Empieza la observación de la aceleración
+	//Empiza la observacion de la aceleracion
 function Comienza(){
+		
+		//Actualiza la aceleracion cada dos segundos
+		//
+var opciones={frequency:2000};
+		
+watchID=navigator.accelerometer.watchAcceleration(Correcto, Error, opciones);
+navigator.geolocation.getCurrentPosition(Localiza,ErrorLocalizacion);
+	}
+	//Detiene la observacion de la aceleracion 
 	
-	//Actualiza la aceleracion cada 2 segundos
-	//
-	var opciones={frequency:2000};
-	
-	watchID=navigator.accelerometer.watchAcceleration(Correcto, Error, opciones);
-	navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
-}
-//Detiene la observacion de la aceleración
-
 function Detente(){
-	if(watchID){
-		navigator .accelerometer.clearWatch (watchID);
-	watchID=null;
-}
-}
-//Correcto:Toma una captura de la aceleracion
+if(watchID){
+navigator .accelerometer.clearWatch (watchID);
+watchID=null;
+		}
+	}
+	//Correcto:Toma una captura de la aceleracion
 function Correcto(acceleration){
-	var element=document.getElementById('acelerometro');
+var element=document.getElementById('acelerometro');
+		
+element.innerHTML='Aceleracion en X:'+acceleration.x+'<br/>'+
+'Aceleracion en Y:'+acceleration.y+'<br/>'+
+'Intervalo:'+acceleration.timestamp+'<br/>';
+	}
 	
-	element.innerHTML='Aceleracion en X:'+acceleration.x+'<br/>'+
-	'Aceleracion en Y:'+acceleration.y+'<br/>'+
-	'Intervalo:'+acceleration.timestamp+'<br/>';
-}
-
-//eRROR FALLA al obtener la aceleracion
+	//eRROR:FALLA aL obtener la aceleracion
 function Error(){
-	alert('Érror!');
-}
-//Exito al Localizar
+alert('Error!');
+	}
+	//Exito al localizar
 function Localiza(posicion){
-	var element=document.getElementById('geolocalizacion');
-	element.innerHTML='Latitud:'+posicion.coords.latitude +'<br/>'+
-	'Longitud:'+posicion.coords.longitude +'<br/>'+
-	'Precision:'+posicion.coords.accuracy+'<br/>'+
-	'Intervalo:'+posicion.timestamp +'<br/>';
-}
-//Error en la geolocalizacion
+var element=document.getElementById('geolocalizacion');
+element.innerHTML='Latitud:' +posicion.coords.latitude+'<br/>'+
+'Longitud:' +posicion.coords.longitude+'<br/>'+
+'Precision:' +posicion.coords.accuracy+'<br/>'+
+'Intervalo:' +posicion.timestamp+'<br/>';
+	}
+	//Error en la geolocalizacion
 function ErrorLocalizacion(error){
-	alert('codigo: '+error.code +'\n'+
-	'mensaje: '+error.message+'\n');
-}
+alert('codigo:'+error.code+'\n'+
+'mensaje:'+error.message+'\n');
+		}
 });//document ready
-
-
-
